8993d92b0728e280a0d9779f435b895bf271916d
Ditipriya Gupta <96857011+diti21@users.noreply.github.com>
2023-04-16 16:55:30 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,10 @@ app.use("/api/notes", noteRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"../frontend/build/index.html")));
+  app.use(express.static(path.join(__dirname,"frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend","build","index.html"))
   );
 } else {
   app.get("/", (req, res) => {
