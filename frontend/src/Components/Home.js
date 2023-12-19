e5e5d80c7b7babee37247a72bce56d800e140ce1
Ditipriya Gupta <mail.ditipriyagupta@gmail.com>
2023-12-19 21:31:38 +0530

    Changed the UI

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -13,11 +13,14 @@ import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 import { styled } from "@mui/material/styles";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material";
 export default function Home() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
-  const { notes } = useSelector((state) => state.noteList);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.up("md"));
+  const { notes = [] } = useSelector((state) => state.noteList);
   const { error: errorDelete, success: successDelete } = useSelector(
     (state) => state.noteDelete
   );
@@ -44,45 +47,122 @@ export default function Home() {
     },
   }));
 
-  return notes?.map((note) => (
-    <Container sx={{ maxWidth: "md", width: "50%" }}>
-      <Card
-        sx={{
-          marginTop: "2rem",
-          boxShadow: "2px 2px 10px 2px rgb(0 0 0 / 20%)",
-        }}
-      >
-        <CardContent sx={{ textAlign: "center" }}>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            sx={{ fontWeight: 600 ,color:"rgb(84, 187, 87) "}}
-          >
-            {note.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {note.content}
-          </Typography>
-        </CardContent>
-        <CardActions sx={{ justifyContent: "center", alignItems: "center" }}>
-          <Link style={{ textDecoration: "none" }} to={`/note/${note._id}`}>
-            <BootstrapTooltip title="Edit">
-              <Button sx={{ color: "green", underline: "none" }}>
-                <EditIcon sx={{ fontSize: 30 }} />
-              </Button>
-            </BootstrapTooltip>
-          </Link>
-          <BootstrapTooltip title="Delete">
-            <Button
-              onClick={() => deleteHandler(note._id)}
-              sx={{ color: "#c62828", mR: "80px" }}
+  return (
+    <>
+      {notes.length === 0 ? (
+        <>
+          {isMobile ? (
+            <Container
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "70vh",
+                maxWidth: "sm",
+                textAlign: "center",
+              }}
             >
-              <DeleteIcon sx={{ fontSize: 30 }} />
-            </Button>
-          </BootstrapTooltip>
-        </CardActions>
-      </Card>
-    </Container>
-  ));
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/9771/9771890.png"
+                style={{ width: "250px" }}
+              />
+              <Typography variant="h5" sx={{ mt: 2, fontWeight: "bold" }}>
+                No note found
+              </Typography>
+            </Container>
+          ) : (
+            <Container
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "70vh",
+                maxWidth: "sm",
+                textAlign: "center",
+              }}
+            >
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/9771/9771890.png"
+                style={{ width: "150px" }}
+              />
+              <Typography variant="h6" sx={{ mt: 2, fontWeight: "bold" }}>
+                No note found
+              </Typography>
+            </Container>
+          )}
+        </>
+      ) : (
+        notes?.map((note) => (
+          <Container
+            sx={{
+              maxWidth: "lg",
+              width: "50%",
+              [theme.breakpoints.down("md")]: {
+                width: "70%",
+              },
+              [theme.breakpoints.down("sm")]: {
+                width: "70%",
+              },
+              [theme.breakpoints.down("xs")]: {
+                width: "100%",
+              },
+            }}
+          >
+            <Card
+              sx={{
+                margin: "2rem auto",
+                boxShadow: "2px 2px 10px 2px rgb(0 0 0 / 20%)",
+              }}
+            >
+              <CardContent sx={{ textAlign: "center" }}>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  sx={{
+                    wordWrap: "break-word",
+                    fontWeight: 600,
+                    color: "rgb(84, 187, 87) ",
+                  }}
+                >
+                  {note.title}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{ wordWrap: "break-word" }}
+                >
+                  {note.content}
+                </Typography>
+              </CardContent>
+              <CardActions
+                sx={{ justifyContent: "center", alignItems: "center" }}
+              >
+                <Link
+                  style={{ textDecoration: "none" }}
+                  to={`/note/${note._id}`}
+                >
+                  <BootstrapTooltip title="Edit">
+                    <Button sx={{ color: "green", underline: "none" }}>
+                      <EditIcon sx={{ fontSize: 30 }} />
+                    </Button>
+                  </BootstrapTooltip>
+                </Link>
+                <BootstrapTooltip title="Delete">
+                  <Button
+                    onClick={() => deleteHandler(note._id)}
+                    sx={{ color: "#c62828", mR: "80px" }}
+                  >
+                    <DeleteIcon sx={{ fontSize: 30 }} />
+                  </Button>
+                </BootstrapTooltip>
+              </CardActions>
+            </Card>
+          </Container>
+        ))
+      )}
+    </>
+  );
 }
